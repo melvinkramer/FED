081652f929e0f49547097937c2557b94e116c9ae
melvinkramer <39735779+melvinkramer@users.noreply.github.com>
2023-10-05 17:53:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,29 @@
-// JavaScript Document
-console.log("hi");
+document.addEventListener("DOMContentLoaded", function () {
+    let items = document.querySelectorAll("#banner li");
+    let currentIndex = 0;
+
+    // Function to hide all items
+    function hideAllItems() {
+        items.forEach(function(item) {
+            item.style.display = 'none';
+        });
+    }
+
+    // Function to show the current item
+    function showCurrentItem() {
+        hideAllItems();
+        items[currentIndex].style.display = 'block';
+    }
+
+    // Initially show the first item
+    showCurrentItem();
+
+    // Change the item every 3 seconds
+    setInterval(function() {
+        currentIndex++;
+        if (currentIndex >= items.length) {
+            currentIndex = 0;
+        }
+        showCurrentItem();
+    }, 3000);
+});
