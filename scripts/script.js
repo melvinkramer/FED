8a65be8627b94697bd6fbc60d2279279dff7174d
melvinkramer <39735779+melvinkramer@users.noreply.github.com>
2023-11-22 16:52:33 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // Banner Carrousel: Deze code creëert een eenvoudige beeldcarrousel die automatisch door verschillende items (lijsten) bladert.
-
-// Bron 1 - Door middel van Chat GPT heb ik een basiscode gekregen en passend gemaakt in mijn code
-
+// Bron 1: ChatGPT als antwoord op de vraag "Hoe maak ik een diavoorstelling in Javascript binnen een webpagina?"
 document.addEventListener("DOMContentLoaded", function () {
     let items = document.querySelectorAll("#banner li"); // Selecteer alle lijstitems
     let currentIndex = 0; // Huidige index van het getoonde item
@@ -9,14 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     // Verberg alle items
     function hideAllItems() {
         items.forEach(function (item) {
-            item.style.display = 'none';
+            item.classList.remove("show-item");
         });
     }
 
     // Toon het huidige item
     function showCurrentItem() {
         hideAllItems();
-        items[currentIndex].style.display = 'block';
+        items[currentIndex].classList.add("show-item");
     }
 
     // Toon het eerste item bij het laden van de pagina
@@ -33,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Video Pauze/Toggle: Deze code zorgt ervoor dat een video kan worden afgespeeld en gepauzeerd door te klikken op een knop.
-// Bron 2 - Door middel van Chat GPT heb ik een basiscode gekregen en een pauze/play button toegevoegd aan de video.
+// Bron 2: ChatGPT als antwoord op de vraag "Hoe voeg ik een play pauze optie toe aan Js voor een video uit de HTML?"
 document.getElementById("videoToggleButton").addEventListener("click", toggleVideo);
 
 function toggleVideo() {
@@ -43,48 +41,44 @@ function toggleVideo() {
 
     if (video.paused) {
         video.play(); // Speel de video af
-        playIcon.style.display = "none"; // Verberg het afspeelicoon
-        pauseIcon.style.display = "inline-block"; // Toon het pauzeicoon
+        playIcon.classList.remove("show-icon"); // Verberg het afspeelicoon
+        pauseIcon.classList.add("show-icon"); // Toon het pauzeicoon
     } else {
         video.pause(); // Pauzeer de video
-        playIcon.style.display = "inline-block"; // Toon het afspeelicoon
-        pauseIcon.style.display = "none"; // Verberg het pauzeicoon
+        playIcon.classList.add("show-icon"); // Toon het afspeelicoon
+        pauseIcon.classList.remove("show-icon"); // Verberg het pauzeicoon
     }
 }
 
+// Open/close menu - Deze code zorgt ervoor dat het sidemenu kan worden geopend en gesloten, in combinatie met een overlay. Hier mogen vragen over gestelt worden!
 
-// Wacht tot de document volledig geladen is
-document.addEventListener("DOMContentLoaded", function () {
-    // Navigatiemenu: Deze code opent en sluit een zijmenu met behulp van een overlay.
-
-    // Voeg een click event listener toe aan de open menu knop
-    document.getElementById("openMenuButton").addEventListener("click", function () {
-        openMenu();
-    });
-
-    // Voeg een click event listener toe aan de close menu knop
-    document.getElementById("closeMenuButton").addEventListener("click", function () {
-        closeMenu();
-    });
-
-    // Optioneel: Voeg een click event listener toe aan de overlay om het menu te sluiten
-    document.getElementById("overlay").addEventListener("click", function () {
-        closeMenu();
-    });
-});
-
+// Bron 3: ChatGPT als antwoord op de vraag "Hoe voeg ik een openen/sluiten menufunctionaliteit toe aan JavaScript-knoppen met een overlay?"
 function openMenu() {
     const menu = document.getElementById("sideMenu");
     const overlay = document.getElementById("overlay");
 
-    menu.style.display = "block"; // Toon het zijmenu
-    overlay.style.display = "block"; // Toon de overlay
+    menu.classList.add("show-menu"); // Voeg de CSS-klasse toe om het zijmenu te tonen
+    overlay.classList.add("show-overlay"); // Voeg de CSS-klasse toe om de overlay te tonen
 }
 
 function closeMenu() {
     const menu = document.getElementById("sideMenu");
     const overlay = document.getElementById("overlay");
 
-    menu.style.display = "none"; // Verberg het zijmenu
-    overlay.style.display = "none"; // Verberg de overlay
+    menu.classList.remove("show-menu"); // Verwijder de CSS-klasse om het zijmenu te verbergen
+    overlay.classList.remove("show-overlay"); // Verwijder de CSS-klasse om de overlay te verbergen
 }
+
+// Voeg event listeners toe aan de knoppen
+document.getElementById("openMenuButton").addEventListener("click", function () {
+    openMenu();
+});
+
+document.getElementById("closeMenuButton").addEventListener("click", function () {
+    closeMenu();
+});
+
+// Optioneel: Voeg een event listener toe aan de overlay om het menu te sluiten
+document.getElementById("overlay").addEventListener("click", function () {
+    closeMenu();
+});
